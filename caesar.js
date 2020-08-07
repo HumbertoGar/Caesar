5dd2da86e694b0608327122dab2773bb54f5130d
humberto <thekingtony96@gmail.com>
2020-08-07 10:16:55 -0400

    Commit

---
+++ caesar.js
@@ -0,0 +1,27 @@
+
+
+const caesar = function(str, shift){
+    return str
+    .split("")
+    .map(select => charShifed(select, shift))
+    .join("")
+}
+
+const codeSet = code => (code < 97 ? 65 : 97);
+
+const mod = (n, m) => (n % m + m) % m;
+
+const charShifed = (select, shift) => {
+  const code = select.charCodeAt();
+
+  if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
+    return String.fromCharCode(
+      mod(code + shift - codeSet(code), 26) + codeSet(code)
+    );
+  }
+  return select;
+};
+
+console.log(caesar('Mjqqt, Btwqi!', -5));
+module.exports = caesar
+
